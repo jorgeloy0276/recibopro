9bae9b4a4435ddb33b0558b5c84eb60487b8d713
jorgeloy0276 <jorgeloy0276@gmail.com>
2026-05-23 00:07:01 -0500

    Add files via upload

---
+++ app/services/email_notifications.py
@@ -0,0 +1,39 @@
+from idlelib import config
+
+from flask_mail import Mail, Message
+from app import mail  # Importamos mail desde __init__.py
+from config import Config
+from flask import flash, redirect, render_template, request, url_for
+
+# Only for test QR code (Temporal)
+import  os
+from flask import current_app
+from app.services.pdf_service import PDF_QR_Service
+import qrcode
+
+# End of test
+
+
+
+class EmailNotifications:
+    @staticmethod
+    def send_notification(receipt_number, html_body):
+
+        PDF_QR_Service.generate_qr_image('REC-20260521-999')
+        msg = Message(
+            subject=f'Notificacion de nuevo Recibo # {receipt_number}',
+            sender='kryptonlogicpty@zohomail.com',
+            recipients=['jorgeloy0276@gmail.com', 'kryptonlogicpty@gmail.com'],
+            html =html_body
+        )
+        try:
+            mail.send(msg)
+
+        except Exception as e:
+            flash(f'Hubo un problema al enviar el correo.. Error: {e}', 'danger')
+            return redirect(url_for('receipts.index'))
+
+
+        flash(f'Recibo {receipt_number} enviado correctamente por email.', 'success')
+        return redirect(url_for('receipts.index'))
+
